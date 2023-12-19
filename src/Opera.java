679b6994f4e82398d28565adbd0f4941169312c6
black cat <kaikoliverlima@gmail.com>
2023-12-19 11:31:44 -0300

    operacoes adicionas

---
+++ src/Opera.java
@@ -0,0 +1,16 @@
+public class Opera {
+    public static void main(String []args){
+      operacoes();
+    }
+    public static void operacoes(){
+        int num1=8;
+        int num2=9;
+        int num3=10;
+        int num4=9;
+        int num5=0;
+        num5= num1 + num2 + num3 + num4;
+        num5= num5/4;
+        System.out.println(num5);
+    }
+
+}
